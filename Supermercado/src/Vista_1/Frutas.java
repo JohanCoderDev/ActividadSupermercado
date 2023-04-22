9341895803ef233a0d286c67c39ebde05f174d93
CristhianDPadillaD <130010339+CristhianDPadillaD@users.noreply.github.com>
2023-04-22 17:40:34 -0500

    Se agrega dispose para cerrar la ventana

--- Supermercado/src/Vista_1/Frutas.java
+++ Supermercado/src/Vista_1/Frutas.java
@@ -152,6 +152,7 @@ public class Frutas extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         Presentacion_2 presentacion2 = Presentacion_2.getPresentacion_2();
         presentacion2.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
