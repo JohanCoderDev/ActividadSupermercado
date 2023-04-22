365642d953203e85a5232b769196bc9080591fe7
CristhianDPadillaD <130010339+CristhianDPadillaD@users.noreply.github.com>
2023-04-22 17:42:45 -0500

    Se agrega dispose

--- Supermercado/src/Vista_1/Lacteos.java
+++ Supermercado/src/Vista_1/Lacteos.java
@@ -144,6 +144,7 @@ public class Lacteos extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
     Presentacion_2 presentacion2 = Presentacion_2.getPresentacion_2();
     presentacion2.setVisible(true);
+    this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
